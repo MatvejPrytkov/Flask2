c6dedcb1cafd5280fc86299c01ad3586e382d200
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-02-25 13:22:03 +0300

    Change run.py

---
+++ Hello/config.py
@@ -0,0 +1,12 @@
+class Config:
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SERVER_NAME = '0.0.0.0:8888'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    PORT = 3333

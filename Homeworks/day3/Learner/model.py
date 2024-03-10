1daf787ab41b1543ee40ceec055375ca580fd387
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-03-10 11:40:44 +0300

    Add hw day3 solution with custom field validators

--- Homeworks/day3/Learner/model.py
+++ Homeworks/day3/Learner/model.py
@@ -1,7 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
 
-
+db = SQLAlchemy()
 
 class Learner(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     final_test = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f'Learner({self.name}, {self.final_test})'
--- Homeworks/day3/Learner/schema.py
+++ Homeworks/day3/Learner/schema.py
@@ -1,19 +1,43 @@
-from marshmallow import Schema, fields
+"""
+https://marshmallow.readthedocs.io/en/stable/quickstart.html#validation
+
+=== Warning ===
+Validation occurs on deserialization but not on serialization. 
+To improve serialization performance, data passed to Schema.dump() are considered valid.
+"""
+
+from marshmallow import Schema, fields, validates, ValidationError
 from model import Learner
 
 
+def check(value: str):
+    if len(value) < 3:
+        raise ValidationError(f"{value} too short for field.")
+    return value
+
+
 class LearnerSchema(Schema):
-    uid = fields.Integer()
-    name = fields.String()
+    uid = fields.Integer(required=True)
+    name = fields.String(validate=check)  # Внешняя функция в роли валидатора
     final_test = fields.Boolean()
 
+    @validates("uid")
+    def check_uid(self, n):
+        """Пример метода-валидатора для конкретного поля"""
+        if n < 1:
+            raise ValidationError(f"{n} must be positive")
+
+    @validates("final_test")
     def validate_final_test(self, value):
-        if not isinstance(value, bool):
-            raise ValueError("Поле 'final_test' должно быть типа 'bool'")
+        if not value:
+            raise ValidationError("Поле 'final_test' должно быть 'truthy'")
         return value
 
 
-learner = Learner(name='Vlad', uid=10, final_test=True)
+learner = Learner(name="Vlad", uid=10, final_test=True)
+print(learner)
 schema = LearnerSchema()
-result = schema.dump(learner)
-print(result)
+result = schema.dumps(learner)
+print(result, type(result))
+de_learner = schema.loads(result)
+print(f"{de_learner = }")
